9dc29400f861e9f5bec84f1958f7bd499f3145a5
jwoirhaye <jerem.woirhaye@gmail.com>
2026-02-28 12:56:37 +0100

    build: fix export mode to deploy

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   basePath: "/2026",
-  assetPrefix: "/2026",
+  assetPrefix: "/2026/",
   images: {
     unoptimized: true, 
   },
